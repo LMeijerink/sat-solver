90163520862f3a36bd3f982945b9859e002034c8
Nithin_Holla <nithin.holla7@gmail.com>
2019-02-15 10:25:38 +0100

    Assignment hotfix

--- DavisPutnam.py
+++ DavisPutnam.py
@@ -14,12 +14,12 @@ class DavisPutnam():
                 self.n_clauses += 1
         self.literals = np.array(list(self.literals))
         self.clause_matrix = np.zeros((self.n_clauses, len(self.literals)), dtype=int)
-        self.assign = np.empty(len(self.literals), dtype=int)
+        self.assign = np.zeros(len(self.literals), dtype=int)
         clause_id = 0
         for line in dimacs_str.splitlines():
             if line[0] != "c" and line[0] != "p":
                 clause = np.zeros(len(self.literals), dtype=int)
-                variables = map(np.int, line.split()[:-1])
+                variables = list(map(np.int, line.split()[:-1]))
                 mask = np.in1d(self.literals, np.abs(variables))
                 clause[mask] = np.sign(variables)
                 self.clause_matrix[clause_id, :] = clause
@@ -32,7 +32,7 @@ class DavisPutnam():
         self.assign[pos_literal_indices] = 1
         self.clause_matrix[:, pos_literal_indices] = 0
         neg_literal_indices = np.where(self.clause_matrix[clauses_with_unit_literals, :] == -1)[1]
-        self.assign[neg_literal_indices] = 0
+        self.assign[neg_literal_indices] = -1
         clauses_to_remove = np.where(self.clause_matrix[:, pos_literal_indices] == 1)[0]
         self.clause_matrix = np.delete(self.clause_matrix, clauses_to_remove, axis=0)
         clauses_to_remove = np.where(self.clause_matrix[:, neg_literal_indices] == -1)[0]
@@ -45,7 +45,7 @@ class DavisPutnam():
         clauses_to_remove = np.where(self.clause_matrix[:, pos_literal_indices] == 1)[0]
         self.clause_matrix = np.delete(self.clause_matrix, clauses_to_remove, axis=0)
         neg_literal_indices = np.all(self.clause_matrix <= 0, axis=0)
-        self.assign[neg_literal_indices] = 0
+        self.assign[neg_literal_indices] = -1
         clauses_to_remove = np.where(self.clause_matrix[:, neg_literal_indices] == -1)[0]
         self.clause_matrix = np.delete(self.clause_matrix, clauses_to_remove, axis=0)
 
@@ -69,7 +69,7 @@ class DavisPutnam():
         if self.solve() == False:
             self.clause_matrix = np.array(clause_state)
             self.assign = np.array(assign_state)
-            self.assign[s] = 0
+            self.assign[s] = -1
             clauses_to_remove = np.where(self.clause_matrix[:, s] == -1)[0]
             self.clause_matrix = np.delete(self.clause_matrix, clauses_to_remove, axis=0) # eliminate clauses with negative occurrences
             self.clause_matrix[:, s] = 0 # eliminate positive occurrences
