330f3e3da44de6b7db764764afa115f223d42d93
Nithin_Holla <nithin.holla7@gmail.com>
2019-02-23 20:39:15 +0100

    Experiments script with plots

--- sat_solve.py
+++ sat_solve.py
@@ -56,72 +56,35 @@ def sudokus_to_DIMACS(filename, rules):
     Takes in file of sudokus and returns a list of puzzles in DIMACS format
     :param filename: Filename of sudoku problems
     :param rules: String of sudoku rules
-    :return: String containing rules and sudoku problems in DIMACS format
+    :return: String containing rules and sudoku problems in DIMACS format, and the number of clues in each problem
     """
     puzzles = []
+    n_clues = []
     with open(filename, 'r') as f:
         for line in f.read().splitlines():
             line = np.array(list(line))
             matrix = np.reshape(line, (9, 9))
             clauses = rules
+            clues = 0
             for x in range(len(matrix)):
                 for y in range(len(matrix[x])):
                     if matrix[x, y] != '.':
                         clauses += str(x + 1) + str(y + 1) + matrix[x, y] + ' 0\n'
+                        clues += 1
             puzzles += [clauses]
-    return puzzles
+            n_clues.append(clues)
+    return puzzles, n_clues
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    sudokufile = '1000 sudokus.txt'
-    n_puzzles = 50
+    sudokufile = 'test_sudokus/1000 sudokus.txt'
+    n_puzzles = 30
+    n_runs = 5
 
     # Load in rules, same for every sudoku
     with open('sudoku-rules.txt') as f:
         rules = f.read()
 
-    example_puzzles = sudokus_to_DIMACS(sudokufile, rules)
+    example_puzzles, clues = sudokus_to_DIMACS(sudokufile, rules)
 
-    splits_dp = []
-    splits_lefv = []
-    backtracks_dp = []
-    backtracks_lefv = []
-    splits = 0
-    backtracks = 0
-
-    print("Solving using DP")
-    i = 0
-    for puz in example_puzzles[:n_puzzles]:
-        splits = 0
-        backtracks = 0
-        print("Puzzle number %d" % i)
-        cnf_dp = CNF(puz)
-        sudoku_solver = SATSolver(heuristic=None)
-        sudoku_solver.solve(cnf_dp)
-        splits_dp.append(sudoku_solver.splits)
-        backtracks_dp.append(sudoku_solver.backtracks)
-        i += 1
-    dp_time = time.time() - start_time
-
-    start_time = time.time()
-    print("Solving using LEFV")
-    i = 0
-    for puz in example_puzzles[:n_puzzles]:
-        splits = 0
-        backtracks = 0
-        print("Puzzle number %d" % i)
-        cnf_lefv = CNF(puz)
-        sudoku_solver = SATSolver(heuristic="LEFV")
-        sudoku_solver.solve(cnf_lefv)
-        splits_lefv.append(sudoku_solver.splits)
-        backtracks_lefv.append(sudoku_solver.backtracks)
-        i += 1
-    lefv_time = time.time() - start_time
-
-    print("Time taken by DP to solve: %f seconds" % dp_time)
-    print("Time taken by LEFV to solve: %f seconds" % lefv_time)
-    print("Mean number of splits with DP: ", np.mean(splits_dp))
-    print("Mean number of splits with LEFV: ", np.mean(splits_lefv))
-    print("Mean number of backtracks with DP: ", np.mean(backtracks_dp))
-    print("Mean number of backtracks with LEFV: ", np.mean(backtracks_lefv))
+    pass
